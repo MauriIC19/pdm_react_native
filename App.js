8dbbbda10abd5d1c71d4180f3ef4eaa6d3605db2
Mauricio IC <mauricio_ic19@outlook.com>
2020-03-10 14:30:14 -0600

    sesión 1

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet, Text, View,
+  TouchableOpacity
+} from 'react-native';
+
+import CustomInput from './components/CustomInput';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hola Mundo</Text>
+      
+      <CustomInput placeholder={"Usuario..."} icon={"user"} />
+      <CustomInput placeholder={"Contraseña..."} icon={"lock"} password={true} />
+      
+      <TouchableOpacity style={styles.button_container}>
+        <Text style={styles.button_text}>Iniciar sesión</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -16,4 +28,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button_container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 210,
+    height: 50,
+    backgroundColor: 'black',
+    borderRadius: 5,
+    marginTop: 10
+  },
+  button_text: {
+    color: 'white',
+    fontSize: 20
+  }
 });
---
+++ components/CustomInput.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { View, TextInput, StyleSheet } from 'react-native';
+
+import Icon from '@expo/vector-icons/FontAwesome';
+
+export default function CustomInput(props) {
+    return (
+        <View style={styles.input_container}>
+            <Icon name={props.icon} size={20} />
+            <TextInput style={styles.input} placeholder={props.placeholder} secureTextEntry={props.password}/>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    input_container: {
+        width: '90%',
+        flexDirection: 'row',
+        alignItems: 'center',
+        borderColor: 'black',
+        borderWidth: 1,
+        padding: 5,
+        marginTop: 10
+    },
+    input: {
+        height: 40,
+        fontSize: 20,
+        marginLeft: 10
+    },
+});
